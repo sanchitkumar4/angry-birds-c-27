3f5016994caab4a00c2aee9e545adba9a44c9306
sanchit kumar <satite99@yahoo.com>
2020-12-23 20:46:09 +0530

    made constraint

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -35,6 +36,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(190,150);
+    log6= new Log (100,200,60,PI/2);
+
+    chain=new Chain(bird.body,log6.body)
 
 }
 
@@ -61,4 +65,8 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    
+    chain.display();
+
 }
